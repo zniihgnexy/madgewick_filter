ae6c67dad9660e633f22ae68568dc4c0de8b4cba
zniihgnexy <151135278+zniihgnexy@users.noreply.github.com>
2024-02-26 19:48:23 +0000

    img

--- data_processing/window_data_imu.py
+++ data_processing/window_data_imu.py
@@ -9,13 +9,24 @@ def sliding_window(data, window_size, overlap):
             windows.append(window)
     return windows
 
+def normalize_column(column, min_val, max_val):
+    # Normalize the values in the column based on min and max values
+    return (column - min_val) / (max_val - min_val)
+
 def split_csv_file(filename, path_ori, path, window_size, overlap, label, column_names):
     # Load the original CSV file
     df = pd.read_csv(path_ori + filename)
     
     # Set the sliding window parameters
     window_size = 150  # Number of rows per window
-    overlap = 100       # Number of rows to overlap
+    overlap = 100      # Number of rows to overlap
+    
+    # Normalize the data
+    for col in df.columns:
+        if 'Acc' in col:
+            df[col] = normalize_column(df[col], -1000, 1000)
+        elif 'Gyr' in col:
+            df[col] = normalize_column(df[col], -1000, 1000)
     
     # Apply sliding window to the dataframe
     windowed_data = sliding_window(df, window_size, overlap)
